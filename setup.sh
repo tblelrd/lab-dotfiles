240b2f327a62f9a80e23faf17397aba8e9b6c1ea
jjxc1 <jjxc1@klovia.cs.st-andrews.ac.uk>
2024-10-31 13:47:20 +0000

    Removed neofetch command temporarily because of errors and now using symlinks instead of copying files

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 START=$(date +%s%3N)
+HOME="/cs/home/$USER"
+
 
 clear
 echo Fetching latest version...
 git pull
 clear
 
-echo Copying command to user bin...
-cp ./fishy /cs/home/$USER/usr/bin
+echo Adding symlinks to user bin...
+ln -sf $HOME/dotfiles/fishy /cs/home/$USER/usr/bin
 
 TIME=$(("$(date +%s%3N)" - START))
 echo Done in ${TIME}ms!
