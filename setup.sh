cfdbc0d65e73f8926c38ccbe6a7f417e265e6df8
Jack <getjacked540c@gmail.com>
2024-11-01 12:20:03 +0000

    Changes to setup script

--- setup.sh
+++ setup.sh
@@ -2,11 +2,8 @@
 START=$(date +%s%3N)
 HOME="/cs/home/$USER"
 
-
-clear
 echo Fetching latest version...
 git pull
-clear
 
 echo Adding symlinks to user bin...
 ln -sf $HOME/dotfiles/fishy /cs/home/$USER/usr/bin
