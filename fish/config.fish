396f106dd803caef1e80a275ca1df3c5cdc5747d
Jack <getjacked540c@gmail.com>
2024-11-01 23:35:15 +0000

    added transient starship prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,12 @@
 function fish_greeting
-    # clear
-    command time -f "%Ems" podman run -it --rm macabees/neofetch
+  # clear
+  command time -f "%Ems" podman run -it --rm macabees/neofetch
 end
-
-fish_config prompt choose pythonista
+function starship_transient_prompt_func
+  starship module character
+end
+starship init fish | source
+# fish_config prompt choose pythonista
 alias dojava="javac *.java && java"
 alias stacscheck="/cs/studres/Library/stacscheck/stacscheck"
 set -Ux NETHOME "/cs/home/$USER/"
