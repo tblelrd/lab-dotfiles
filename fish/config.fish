c7503fa852002bc59476c1bb77ab6015accafe40
Jack <getjacked540c@gmail.com>
2024-11-02 00:07:16 +0000

    disabled transince because it doesn't work

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,7 @@ function fish_greeting
   # clear
   command time -f "%Ems" podman run -it --rm macabees/neofetch
 end
-function starship_transient_prompt_func
-  starship module character
-end
 starship init fish --print-full-init | sed 's/"$(commandline)"/(commandline | string collect)/' | source
-enable_transience
 # fish_config prompt choose pythonista
 alias dojava="javac *.java && java"
 alias stacscheck="/cs/studres/Library/stacscheck/stacscheck"
