b5c5318b4649cdc6109b68744e31892ebe3438e6
Jack <tblelrd@gmail.com>
2025-02-26 15:29:33 +0000

    Test

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,16 @@
 function fish_greeting
   # clear
   # command time -f "%Ems" podman run -it --rm macabees/neofetch
+  fastfetch
 end
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
 starship init fish --print-full-init | sed 's/"$(commandline)"/(commandline | string collect)/' | source
 # fish_config prompt choose pythonista
 alias dojava="javac *.java && java"
