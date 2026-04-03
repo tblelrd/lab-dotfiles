1e31a28e690cc2dcb75dabb9836e70b162c9fa7b
Jack from laptop <getjacked540c@gmail.com>
2026-04-03 14:18:48 +0100

    Updated nvim and added lazygit?

--- configs/nvim/lua/polish.lua
+++ configs/nvim/lua/polish.lua
@@ -3,6 +3,12 @@ vim.opt.shiftwidth = 4;
 vim.opt.expandtab = true;
 
 vim.keymap.set("n", "J", "<nop>", { desc = "Disable join lines" })
+
+vim.api.nvim_create_user_command("LspLogClear", function()
+	local lsplogpath = vim.fn.stdpath("state") .. "/lsp.log"
+	print(lsplogpath)
+	if io.close(io.open(lsplogpath, "w+b")) == false then vim.notify("Clearning LSP Log failed.", vim.log.levels.WARN) end
+end, { nargs = 0 })
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
